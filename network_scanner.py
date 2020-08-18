49aa3e815f8058a4f441f127b0eaaa15f63dde2b
Daniil Nababkin <43863412+TheRavehorn@users.noreply.github.com>
2020-08-19 02:06:48 +0300

    Create network_scanner.py

---
+++ network_scanner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import scapy.all as scapy
+
+
+def scan(ip):
+    scapy.arping(ip)
+
+
+print("Simple network scanner 0.01 by Ravehorn.")
+ip_to_scan = input("Type IP range to scan: ")
+scan(ip_to_scan)
