578e3d07f38634cea368f422f76ab59ee882085b
Daniil Nababkin <43863412+TheRavehorn@users.noreply.github.com>
2020-08-23 01:02:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ except ModuleNotFoundError:
     time.sleep(3)
     subprocess.call("apt-get update", shell=True)
     subprocess.call("pip3 install scapy --quiet", shell=True)
-finally:
-    subprocess.call("clear", shell=True)
+# finally:
+    # subprocess.call("clear", shell=True)
