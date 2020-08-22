44bdb405e99d423d69c20e3b7f95ad47e1b7c139
Daniil Nababkin <43863412+TheRavehorn@users.noreply.github.com>
2020-08-23 01:07:22 +0300

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import setup
 import scapy.all as scapy
+import subprocess
 
 
 def scan(ip):
@@ -8,5 +9,6 @@ def scan(ip):
 
 
 print("Simple network scanner 0.01 by Ravehorn.")
+subprocess.call("ifconfig", shell=True)
 ip_to_scan = input("Type IP range to scan: ")
 scan(ip_to_scan)
